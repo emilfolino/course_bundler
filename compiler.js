5547f9a369f1441521a68a035551f345c04389bb
Emil Folino <emilfolino@gmail.com>
2023-09-15 08:37:57 +0200

    Worked on it shortly

--- compiler.js
+++ compiler.js
@@ -13,3 +13,36 @@ const Entities = require('html-entities').AllHtmlEntities;
 const entities = new Entities();
 
 const courses = require("courses-config.json");
+
+const includes = "./includes/";
+const output = "./output/";
+const mainTitle = "nya-dbwebb.se";
+
+
+function createHeader(next) {
+    fs.readFile(`${includes}header.html`, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err.message);
+        }
+
+        fs.readFile(`${output}style.min.css`, 'utf8', (err, cssData) => {
+            if (err) {
+                console.error(err.message);
+            }
+
+            next(data.replace("{{inline-style}}", cssData));
+        });
+    });
+}
+
+function createIndex(header) {
+    fs.readFile(`${includes}index.html`, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err.message);
+        }
+
+        let outputContent = header.replace("{{title}}", mainTitle) + data;
+
+        compiler.addFooterAndWriteToFile(outputContent, "index.html");
+    });
+}
